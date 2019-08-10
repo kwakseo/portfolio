553cdd157143444035b44ec9474cac642841bde3
Rachel Kwak <kwakseo@mit.edu>
2019-08-10 15:22:54 -0400

    ajax test

--- load_page.php
+++ load_page.php
@@ -2,10 +2,10 @@
 
 if(!$_POST['page']) die("0");
 
-$page = $_POST['page'] + ".html";
+$page = $_POST['page'];
 
-if(file_exists($page))
-echo file_get_contents($page);
+if(file_exists($page.'.html'))
+echo file_get_contents($page.'.html');
 
 else echo 'There is no such page!';
 
