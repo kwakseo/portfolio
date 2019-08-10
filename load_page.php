0ad3308e18afe8cb32d0ce02a308a1936528d66f
Rachel Kwak <kwakseo@mit.edu>
2019-08-09 22:35:28 -0400

    php tweaks

--- load_page.php
+++ load_page.php
@@ -2,7 +2,7 @@ if(!$_POST['page']) die("0");
 
 $page = (int)$_POST['page'];
 
-if(file_exists(.$page.'.html'))
-echo file_get_contents(.$page.'.html');
+if(file_exists($page'.html'))
+echo file_get_contents($page'.html');
 
 else echo 'There is no such page!';
