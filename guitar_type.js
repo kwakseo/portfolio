8506d66aae579109e6730037e6ba6a4233d02ddf
Rachel Kwak <kwakseo@mit.edu>
2019-10-27 18:26:57 -0400

    guitar no letter case

--- guitar_type.js
+++ guitar_type.js
@@ -7,30 +7,36 @@ function playAudio() {
 	var index = 0;
 	var audio = new Audio();
 
-	audio.src = "audio/mp3files/fade/" + text[0] + ".mp3";
-	index = index + 1;
-	document.getElementById("text_input").style.display = "none";
-	text_animation.style.display = "flex";
-	text_animation.style.color="#cccccc";
-	text_animation.innerHTML = '<span style="color: black;">'+text[0]+'</span>' + text.substring(1, num_letters);
-	
-	audio.play();
+	if (num_letters > 0) {
+		
+		audio.src = "audio/mp3files/fade/" + text[0] + ".mp3";
+		index = index + 1;
+		document.getElementById("text_input").style.display = "none";
+		text_animation.style.display = "flex";
+		text_animation.style.color="#cccccc";
+		text_animation.innerHTML = '<span style="color: black;">'+text[0]+'</span>' + text.substring(1, num_letters);
+		
+		audio.play();
 
-	audio.onended = function() {
+		audio.onended = function() {
+
 	    if(index < num_letters) {
+
 	        text_animation.innerHTML = text.substring(0,index) + '<span style="color: black;">'+text[index]+'</span>' + text.substring(index+1,num_letters);
 	        audio.src= "audio/mp3files/fade/" + text[index] + ".mp3";
 	        audio.play();
 	        index++;
+
 	    }
 
 	    else {
 	    	document.getElementById("text_input").style.display = "flex";
 			text_animation.style.display = "none";
 	    }
+
+		};
+
 	}
-
-
 
 };
 
